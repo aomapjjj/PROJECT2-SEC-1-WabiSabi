f1c9290d89c9b3fa902a44ce8e90b730bf994a2c
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-10-12 02:17:43 +0700

    050-[add] pinia to set user name in nav bar , fix bug

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup>
+import { useUsers } from '@/stores/userStore'
+
+const userStore = useUsers()
+const userName = userStore.getUser().username
+
+</script>
 
 <template>
   <header class="bg-white shadow-md">
@@ -63,28 +69,7 @@
           </button>
         </div>
         <div class="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
-          <button
-            type="button"
-            class="relative flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <span class="absolute -inset-1.5"></span>
-            <span class="sr-only">View notifications</span>
-            <svg
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-              />
-            </svg>
-          </button>
-
+        <h1>Hello, {{ userName }}</h1>
           <!-- Profile dropdown -->
           <div class="relative ml-4 flex-shrink-0">
             <div>
