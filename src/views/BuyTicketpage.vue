49ccfed39c52e7f8d09998eabd483b1aafba27ef
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-10-18 16:30:43 +0700

    050-[update]-use userStore แต่ละ Components

--- src/views/BuyTicketpage.vue
+++ src/views/BuyTicketpage.vue
@@ -1,19 +1,22 @@
 <script setup>
-import Buyticketmodal from '@/components/Buyticketmodal.vue';
-import { ref ,  onMounted , watch } from 'vue';
+import Buyticketmodal from "@/components/Buyticketmodal.vue"
+import { ref, onMounted, watch } from "vue"
 
-import { getItemById } from '../../libs/fetchUtils'
+import { getItemById } from "../../libs/fetchUtils"
 import { useRouter, useRoute } from "vue-router"
+import { useUsers } from "@/stores/userStore"
 
+const userStore = useUsers()
+const userInfo = userStore.getUser()
 const route = useRoute()
 const router = useRouter()
 
+console.log("userInfo", userInfo)
 
 const baseUrlconcert = `${import.meta.env.VITE_APP_URL_CON}`
 const itembyId = ref()
 
-
-const buyticketItemId = ref('')
+const buyticketItemId = ref("")
 
 watch(
   () => route.params.buyticketId,
@@ -24,29 +27,26 @@ watch(
 )
 
 onMounted(async () => {
-  try{
-    const item  = await getItemById(baseUrlconcert,buyticketItemId.value)
-  itembyId.value = item
-    console.log( itembyId.value )
-  }catch(error){
-    console.log('error na')
+  try {
+    const item = await getItemById(baseUrlconcert, buyticketItemId.value)
+    itembyId.value = item
+    console.log(itembyId.value)
+  } catch (error) {
+    console.log("error na")
   }
-}) 
+})
 
-
-const couter = ref(1);
+const couter = ref(1)
 
 const updateCouter = (newCouter) => {
-  couter.value = newCouter;
-};
-
-
+  couter.value = newCouter
+}
 </script>
 
 <template>
-   <Buyticketmodal @update:couter="updateCouter">
+  <Buyticketmodal @update:couter="updateCouter">
     <template #imgOfTicket>
-    <img :src="itembyId?.img" alt="">
+      <img :src="itembyId?.img" alt="" />
     </template>
     <template #title>
       {{ itembyId?.title }}
@@ -55,23 +55,29 @@ const updateCouter = (newCouter) => {
       {{ itembyId?.location }}
     </template>
     <template #subtotal>
-      {{ (itembyId?.price * couter).toFixed(2) == 0 ? 'Free' : (itembyId?.price * couter).toFixed(2) }}
+      {{
+        (itembyId?.price * couter).toFixed(2) == 0
+          ? "Free"
+          : (itembyId?.price * couter).toFixed(2)
+      }}
     </template>
     <template #tax>
       {{ parseFloat((itembyId?.price * couter * (7 / 100)).toFixed(2)) }}
     </template>
     <template #total>
-      {{ (itembyId?.price * couter + parseFloat((itembyId?.price * couter * (7 / 100)).toFixed(2))).toFixed(2) }}
+      {{
+        (
+          itembyId?.price * couter +
+          parseFloat((itembyId?.price * couter * (7 / 100)).toFixed(2))
+        ).toFixed(2)
+      }}
     </template>
     <template #fullname>
-    
+      {{ userInfo.firstname + ' ' + userInfo.lastname  }}
     </template>
-    <template #address>
-    
+    <template #address> 
+      {{ userInfo.address === undefined ? '-' : userInfo.address }}
     </template>
   </Buyticketmodal>
-
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
